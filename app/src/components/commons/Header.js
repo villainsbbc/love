5e68a444345c7ef49f264526a9fff7cef069d8ae
terumi-s22 <112700063+terumi-s22@users.noreply.github.com>
2023-04-17 20:53:45 +0900

    コード修正

--- app/src/components/commons/Header.js
+++ app/src/components/commons/Header.js
@@ -1,29 +1,5 @@
 import React from "react";
-import { AppBar, Link, Typography, Grid } from "@mui/material";
-
-const customStyles = {
-  root: {
-    direction: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  img: {
-    height: "100%",
-    width: "70%",
-  },
-  title: {
-    fontSize: "3vw",
-    fontWeight: "bold",
-  },
-  sns: {
-    textAlign: "right",
-  },
-  snsLogo: {
-    height: "100%",
-    width: "25%",
-    margin: "1.5%",
-  },
-};
+import { Box, Link, Typography, Grid } from "@mui/material";
 
 const snsImages = [
   {
@@ -46,35 +22,54 @@ const snsImages = [
 const Header = () => {
   return (
     <>
-      <AppBar position="static" color="default">
-        <Grid container style={customStyles.root}>
-          <Grid item xs={2}>
-            <Link href="/home">
-              <img
-                src="/images/logo.png"
-                alt="チームロゴ"
-                style={customStyles.img}
+      <Grid
+        container
+        sx={{
+          direction: "column",
+          alignItems: "center",
+          justifyContent: "center" 
+        }}
+      >
+        <Grid item xs={2}>
+          <Link href="/home">
+            <Box
+              component="img"
+              src="/images/logo.png"
+              alt="チームロゴ"
+              sx={{
+                height: "100%",
+                width: "70%"
+              }}
+            />
+          </Link>
+        </Grid>
+        <Grid item xs={7}>
+          <Typography
+            sx={{
+              fontSize: "3vw",
+              fontWeight: "bold"
+            }}
+          >
+            女子軟式野球チーム Villains(ヴィランズ)
+          </Typography>
+        </Grid>
+        <Grid item xs={3} sx={{ textAlign: "right" }}>
+          {snsImages.map((item) => (
+            <Link href={item.href} target="_blank" rel="noreferrer">
+              <Box
+                component="img"
+                src={item.src}
+                alt={item.alt}
+                sx={{
+                  height: "100%",
+                  width: "25%",
+                  margin: "1.5%",
+                }}
               />
             </Link>
-          </Grid>
-          <Grid item xs={7}>
-            <Typography style={customStyles.title}>
-              女子軟式野球チーム Villains(ヴィランズ)
-            </Typography>
-          </Grid>
-          <Grid item xs={3} style={customStyles.sns}>
-            {snsImages.map((item) => (
-              <Link href={item.href} target="_blank" rel="noreferrer">
-                <img
-                  src={item.src}
-                  alt={item.alt}
-                  style={customStyles.snsLogo}
-                />
-              </Link>
-            ))}
-          </Grid>
+          ))}
         </Grid>
-      </AppBar>
+      </Grid>
     </>
   );
 };
